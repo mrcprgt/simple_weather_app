5c81116d345666e4ff08fe08b71c0222a4abbe4e
Marc Christian E. Pragata <mpragata@gmail.com>
2020-06-23 17:38:34 +0800

    initial http requests working

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -7,7 +10,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Variables
   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  String address;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -36,10 +41,28 @@ class _HomeState extends State<Home> {
                     ),
                     MaterialButton(
                       child: Text("Submit"),
-                      onPressed: () {
-                        if (_fbKey.currentState.saveAndValidate()) {
-                          print(_fbKey.currentState.value);
-                        }
+                      onPressed: () async {
+                        _fbKey.currentState.saveAndValidate();
+
+                        //Combine values from textfields and trim leading whitespace
+                        address = _fbKey.currentState.value["brgy"].trim() +
+                            ", " +
+                            _fbKey.currentState.value["city"].trim() +
+                            ", " +
+                            _fbKey.currentState.value["prov"].trim();
+
+                        print(address);
+
+                        //make an http request
+                        var url =
+                            'https://us1.locationiq.com/v1/search.php?key=pk.03ce5820ab6a126c25d2e02370c966fd&q=$address&format=json&matchquality=[0|1]';
+                        var response = await http.post(url);
+                        //print('Response status: ${response.statusCode}');
+                        var jsonData = response.body;
+                        var parsedJson = json.decode(jsonData);
+                        print('${parsedJson.runtimeType} : $parsedJson');
+                        print(parsedJson[0]["lat"]);
+                        print(parsedJson[0]["matchcode"]);
                       },
                     ),
                   ],
