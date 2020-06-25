41afb312eb66b8327d08120ea75210d1fe07b60c
Marc Christian E. Pragata <mpragata@gmail.com>
2020-06-25 12:38:58 +0800

    changed to support page transitions

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:simple_weather_app/home.dart';
 import 'package:simple_weather_app/weatherscreen.dart';
 import 'mapscreen.dart';
@@ -13,25 +14,43 @@ class Router {
       case '/':
         return MaterialPageRoute(builder: (_) => Home());
         break;
+      // case '/mapscreen':
+      //   if (args is LatLng) {
+      //     return MaterialPageRoute(
+      //       builder: (_) => MapScreen(
+      //         data: args,
+      //       ),
+      //     );
+      //   }
+      //   break;
       case '/mapscreen':
         if (args is LatLng) {
-          return MaterialPageRoute(
-            builder: (_) => MapScreen(
-              data: args,
-            ),
+          return PageTransition(
+            child: MapScreen(data: args),
+            type: PageTransitionType.rightToLeft,
+            settings: settings,
           );
         }
         break;
 
+      //   case '/weatherscreen':
+      //     if (args is LatLng) {
+      //       return MaterialPageRoute(
+      //         builder: (_) => WeatherScreen(
+      //           data: args,
+      //         ),
+      //       );
+      //     }
+      //     break;
+      // }
       case '/weatherscreen':
         if (args is LatLng) {
-          return MaterialPageRoute(
-            builder: (_) => WeatherScreen(
-              data: args,
-            ),
+          return PageTransition(
+            child: WeatherScreen(data: args),
+            type: PageTransitionType.rightToLeft,
+            settings: settings,
           );
         }
-        break;
     }
   }
 }
